3ef54eb3214f28ed2bd388a5a851c5aa375f90c9
SarahGathoni <sarah.wanjiku.gathoni@gmail.com>
2022-09-10 01:42:55 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a hexadecimal string
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		putchar(a);
+	}
+	for (a = 'a'; a <= 'f'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
