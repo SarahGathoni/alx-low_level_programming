e269ac32deebf84c8bbc9fd5ffe9ff31a9bc21fb
SarahGathoni <sarah.wanjiku.gathoni@gmail.com>
2022-09-13 14:07:03 -0700

    main.h

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,19 @@
-#include <stdio.h>
+#include <main.h>
 
 /**
- * main - prints _putchar followed by a new line
+ * Description: prints _putchar, followed by a new line
  *
  * Return: always 0
  */
 int main(void)
 {
-	_putchar('_putchar/n');
+	char text[9] = "_putchar";
+	int i = 0;
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(text[i]);
+	}
+	_putchar('\n');
 	return (0);
 }
+
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "_putchar.c"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+	return (0);
+}
