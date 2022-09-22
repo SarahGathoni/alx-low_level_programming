ab2b3819a6b603a1ec43d01590b53bcae5d925b1
SarahGathoni <sarah.wanjiku.gathoni@gmail.com>
2022-09-22 11:38:58 -0700

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,11 +2,12 @@
 #include <stdio.h>
 
 /**
- * *_strncpy - cpoies n bytes of src to the dest string
+ * _strncpy - cpoies n bytes of src to the dest string
  * @dest: string to copy or destination
  *  @src: string being copied or source
  *  @n: largest number of bytes to copy
  *
+ * Return: pointer to the resulting string
  */
 char *_strncpy(char *dest, char *src, int n)
 {
