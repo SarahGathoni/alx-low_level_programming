01b390923d08286ea65c7c1183657f50fa2f69fd
SarahGathoni <sarah.wanjiku.gathoni@gmail.com>
2022-10-03 07:17:44 -0700

    101-strtow.c

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -6,3 +6,5 @@ int **alloc_grid(int width, int height);
 char *str_concat(char *s1, char *s2);
 char *_strdup(char *str);
 char *create_array(unsigned int size, char c);
+int count_words(char *str);
+int word_len(char *str);
