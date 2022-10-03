19f1bbbb082296d19f2d8db3444a83e4a4c7548e
SarahGathoni <sarah.wanjiku.gathoni@gmail.com>
2022-10-03 07:34:11 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -2,9 +2,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
- *count_words - Counts the number of words contained within a string.
- *@str: The string to be searched.
+int word_len(char *str);
+int count_words(char *str);
+char **strtow(char *str);
+
+/**
+ * word_len - Locates the index marking the end of the
+ *            first word contained within a string.
+ * @str: The string to be searched.
+ *
+ * Return: The index marking the end of the initial word pointed to by str.
+ */
+int word_len(char *str)
+{
+	int index = 0, len = 0;
+
+	while (*(str + index) && *(str + index) != ' ')
+	{
+		len++;
+		index++;
+	}
+
+	return (len);
+}
+
+/**
+ * count_words - Counts the number of words contained within a string.
+ * @str: The string to be searched.
  *
  * Return: The number of words contained within str.
  */
@@ -30,7 +54,7 @@ int count_words(char *str)
  * Return: If str = NULL, str = "", or the function fails - NULL.
  *         Otherwise - a pointer to an array of strings (words).
  */
-**strtow(char *str)
+char **strtow(char *str)
 {
 	char **strings;
 	int index = 0, words, w, letters, l;
