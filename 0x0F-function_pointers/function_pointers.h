1f60fc1bc9aeaed7a1e602b1914bbd03182a5e63
SarahGathoni <sarah.wanjiku.gathoni@gmail.com>
2022-10-14 22:03:23 -0700

    function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,12 +1,10 @@
+#ifndef FUNC_H
+#define FUNC_H
+#include <stdlib.h>
+
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-int (*get_op_func(char *s))(int, int);
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
-int main(int argc, char **argv);
-void print_opcodes(char *a, int n);
+
+#enfif
